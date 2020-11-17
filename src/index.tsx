33679069c40611bfd99b79c7563d3bf089e77e35
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-17 15:41:40 +0200

    Get rrweb rendering

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,67 @@ import React, { createRef, PureComponent, RefObject } from 'react'
 import Slider from 'rc-slider/lib/Slider'
 import { FaPlay } from 'react-icons/fa'
 import { IconContext } from 'react-icons'
+import { Replayer } from 'rrweb'
+import events from './recording'
 
 import './styles.css'
 import 'rc-slider/assets/index.css'
+import 'rrweb/dist/rrweb.min.css'
 
 interface Props {}
 
 export class Player extends PureComponent<Props> {
     frame: RefObject<HTMLDivElement> = createRef()
+    replayer: Replayer
+
+    componentDidMount() {
+        if (this.frame.current) {
+            this.replayer = new Replayer(events, {
+                root: this.frame.current,
+                speed: 4,
+                skipInactive: true
+            })
+
+            this.replayer.play()
+
+            this.replayer.on('resize', this.updatePlayerDimensions)
+
+            // @ts-ignore
+            window.replayer = this.replayer
+
+            const addHandler = (event: string) => {
+                this.replayer.on(event, (...args: any[]) =>
+                    console.log('replayer event:', event, args)
+                )
+            }
+
+            addHandler('start')
+            addHandler('pause')
+            addHandler('resume')
+            addHandler('resize')
+            addHandler('finish')
+            addHandler('fullsnapshot-rebuilded')
+            addHandler('load-stylesheet-start')
+            addHandler('load-stylesheet-end')
+            addHandler('skip-start')
+            addHandler('skip-end')
+            addHandler('mouse-interaction')
+            addHandler('event-cast')
+            addHandler('custom-event')
+            addHandler('flush')
+            addHandler('state-change')
+        }
+    }
+
+    updatePlayerDimensions = (replayDimensions: {
+        width: number
+        height: number
+    }) => {
+        const { width } = this.frame.current!.getBoundingClientRect()
+
+        const scale = Math.min(width / replayDimensions.width, 1)
+        this.replayer.wrapper.style.transform = `scale(${scale})`
+    }
 
     render = () => (
         <div className='ph-rrweb-wrapper'>
