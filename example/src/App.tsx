dad7a9d20aa114f9498a1cb2ece1f28f25275d79
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-18 14:26:07 +0200

    Display events from file

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,11 +3,9 @@ import React from 'react'
 import { Player } from 'posthog-react-rrweb-player'
 import 'posthog-react-rrweb-player/dist/index.css'
 
-const events: any = JSON.parse(localStorage.getItem('session')!).result
-
-const App = () => (
+const App = (props: { events: any }) => (
   <div style={{ height: '90vh', width: '90vw' }}>
-    <Player events={events} />
+    <Player events={props.events} />
   </div>
 )
 
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,4 +4,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const render = async(url: string) => {
+    const response = await fetch(url)
+    const { result } = await response.json()
+    ReactDOM.render(<App events={result} />, document.getElementById('root'))
+}
+
+render('/broken-recording.json')
