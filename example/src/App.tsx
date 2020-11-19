31e86a518cbab8191643f380bf151ec4c7453e2d
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-19 15:41:25 +0200

    Make browsing different recordings easier

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Player } from 'posthog-react-rrweb-player'
+import useLocalStorageState from 'use-local-storage-state'
+import Select from 'react-select'
+import { eventWithTime } from 'rrweb/typings/types'
+
 import 'posthog-react-rrweb-player/dist/index.css'
 
-const App = (props: { events: any }) => (
-  <div style={{ height: '90vh', width: '90vw' }}>
-    <Player events={props.events} />
-  </div>
-)
+const makeOption = (value: string) => ({ value, label: value })
+
+const RECORDINGS = [
+  makeOption('/docs.json'),
+  makeOption('/local-docs.json'),
+]
+
+const App = () => {
+  const [events, setEvents] = useState<eventWithTime[]>([])
+  const [recording, setRecording] = useLocalStorageState('recording', RECORDINGS[0])
+  const [activeRecording, setActiveRecording] = useState(recording)
+
+  useEffect(() => {
+      async function fetchRecording() {
+        const response = await fetch(recording.value)
+        const { result } = await response.json()
+
+        setEvents(result)
+        setActiveRecording(recording)
+    }
+
+    fetchRecording()
+  }, [recording])
+
+  return (
+    <div style={{ height: '90vh', width: '90vw' }}>
+      <div style={{ zIndex: 100 }}>
+        <Select
+          options={RECORDINGS}
+          value={recording}
+          onChange={(recording) => setRecording(recording as any)}
+        />
+      </div>
+
+      <br />
+
+      {events.length > 0 && <Player events={events} key={activeRecording.value} />}
+    </div>
+  )
+}
 
 export default App
--- example/src/index.tsx
+++ example/src/index.tsx
@@ -4,10 +4,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-const render = async(url: string) => {
-    const response = await fetch(url)
-    const { result } = await response.json()
-    ReactDOM.render(<App events={result} />, document.getElementById('root'))
-}
-
-render('/local-docs.json')
+ReactDOM.render(<App />, document.getElementById('root'))
