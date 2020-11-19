7c8d914991685bc297a038aebe876568468049e9
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-19 13:50:05 +0200

    Use hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
-import React, { createRef, PureComponent, RefObject } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Slider from 'rc-slider/lib/Slider'
 import { FaBackward, FaPause, FaPlay } from 'react-icons/fa'
 import { IconContext } from 'react-icons'
 import { Replayer } from 'rrweb'
-import { eventWithTime, playerMetaData, viewportResizeDimention } from 'rrweb/typings/types'
+import {
+    eventWithTime,
+    playerMetaData,
+    viewportResizeDimention
+} from 'rrweb/typings/types'
 
 import { formatTime } from './time'
 import { PlayPauseOverlay } from './PlayPauseOverlay'
@@ -18,169 +22,160 @@ interface Props {
     events: eventWithTime[]
 }
 
-interface State {
-    playing: boolean
-    currentTime: number
-    meta: playerMetaData
-}
+export function Player(props: Props) {
+    const [playing, setPlaying] = useState(true)
+    const [currentTime, setCurrentTime] = useState(0)
+    const [meta, setMeta] = useState<playerMetaData>({
+        startTime: 0,
+        endTime: 0,
+        totalTime: 0
+    })
 
-export class Player extends PureComponent<Props, State> {
-    state: State = {
-        playing: true,
-        currentTime: 0,
-        meta: {
-            startTime: 0,
-            endTime: 0,
-            totalTime: 0
-        }
-    }
+    const frame = useRef<HTMLDivElement>(null)
+    const wrapper = useRef<HTMLDivElement>(null)
 
-    frame: RefObject<HTMLDivElement> = createRef()
-    wrapper: RefObject<HTMLDivElement> = createRef()
-    replayer: Replayer
-    replayDimensions: viewportResizeDimention
-    timer: number
+    const replayer = useRef<Replayer>()
+    const replayDimensionRef = useRef<viewportResizeDimention>()
+    const timer = useRef<number>()
 
-    componentDidMount() {
-        if (this.frame.current) {
-            this.replayer = new Replayer(this.props.events, {
-                root: this.frame.current,
-                speed: 4,
+    useEffect(() => {
+        if (frame.current) {
+            replayer.current = new Replayer(props.events, {
+                root: frame.current,
+                speed: 1,
                 skipInactive: true
             })
 
             window.addEventListener('resize', () =>
-                this.updatePlayerDimensions(this.replayDimensions)
+                updatePlayerDimensions(replayDimensionRef.current!)
             )
-            this.replayer.on('resize', this.updatePlayerDimensions)
-            this.replayer.on('start', this.startTimeLoop)
-            this.replayer.on('resume', this.startTimeLoop)
-            this.replayer.on('pause', this.stopTimeLoop)
-            this.replayer.on('finish', this.stopTimeLoop)
-            this.replayer.on('finish', this.pause)
+            replayer.current.on('resize', updatePlayerDimensions)
+            // replayer.current.on('start', startTimeLoop)
+            // replayer.current.on('resume', startTimeLoop)
+            // replayer.current.on('pause', stopTimeLoop)
+            // replayer.current.on('finish', stopTimeLoop)
+            replayer.current.on('finish', pause)
 
-            this.replayer.play()
+            replayer.current.play()
 
-            const meta = this.replayer.getMetaData()
-            this.setState({ playing: true, meta })
-            this.updateTime()
+            const meta = replayer.current.getMetaData()
 
-            this.wrapper.current!.focus()
+            setPlaying(true)
+            setMeta(meta)
+
+            wrapper.current!.focus()
 
             // @ts-ignore
-            window.replayer = this.replayer
+            window.replayer = replayer.current
+        }
 
-            // const addHandler = (event: string) => {
-            //     this.replayer.on(event, (...args: any[]) =>
-            //         console.log('replayer event:', event, args)
-            //     )
-            // }
+        return () => {
+            stopTimeLoop()
+            replayer.current!.pause()
+        }
+    }, [])
 
-            // addHandler('start')
-            // addHandler('pause')
-            // addHandler('resume')
-            // addHandler('resize')
-            // addHandler('finish')
-            // addHandler('fullsnapshot-rebuilded')
-            // addHandler('load-stylesheet-start')
-            // addHandler('load-stylesheet-end')
-            // addHandler('skip-start')
-            // addHandler('skip-end')
-            // addHandler('mouse-interaction')
-            // addHandler('event-cast')
-            // addHandler('custom-event')
-            // addHandler('flush')
-            // addHandler('state-change')
+    useEffect((): any => {
+        stopTimer()
+
+        if (playing && meta.totalTime > 0) {
+            updateTime()
+            return () => stopTimer()
+        }
+    }, [playing, meta])
+
+    const stopTimer = () => {
+        if (timer.current) {
+            cancelAnimationFrame(timer.current)
         }
     }
 
-    updatePlayerDimensions = (replayDimensions: viewportResizeDimention) => {
-        this.replayDimensions = replayDimensions
-        const { width } = this.frame.current!.getBoundingClientRect()
+    const updateTime = () => {
+        const currentTime = Math.min(
+            replayer.current!.getCurrentTime(),
+            meta.totalTime
+        )
+        setCurrentTime(currentTime)
+
+        timer.current = requestAnimationFrame(updateTime)
+    }
+
+    const updatePlayerDimensions = (
+        replayDimensions: viewportResizeDimention
+    ) => {
+        replayDimensionRef.current = replayDimensions
+        const { width } = frame.current!.getBoundingClientRect()
 
         const scale = Math.min(width / replayDimensions.width, 1)
-        this.replayer.wrapper.style.transform = `scale(${scale})`
+        replayer.current!.wrapper.style.transform = `scale(${scale})`
     }
 
-    startTimeLoop = () => {
-        this.stopTimeLoop()
-        this.updateTime()
+    const stopTimeLoop = () => {
+        if (timer.current) {
+            cancelAnimationFrame(timer.current)
+        }
     }
 
-    stopTimeLoop = () => {
-        cancelAnimationFrame(this.timer)
-    }
-
-    handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
         if (event.key === ' ') {
-            if (this.state.playing) {
-                this.pause()
+            if (playing) {
+                pause()
             } else {
-                this.play()
+                play()
             }
         } else if (event.key === 'ArrowLeft') {
-            this.seek(this.state.currentTime - JUMP_TIME_MS / 2)
+            seek(currentTime - JUMP_TIME_MS / 2)
         } else if (event.key === 'ArrowRight') {
-            this.seek(this.state.currentTime + JUMP_TIME_MS / 2)
+            seek(currentTime + JUMP_TIME_MS / 2)
         }
     }
 
-    updateTime = () => {
-        const currentTime = Math.min(
-            this.replayer.getCurrentTime(),
-            this.state.meta.totalTime
-        )
-        this.setState({ currentTime })
-
-        this.timer = requestAnimationFrame(this.updateTime)
+    const play = () => {
+        setPlaying(true)
+        replayer.current!.play(currentTime)
     }
 
-    play = () => {
-        this.setState({ playing: true })
-        this.replayer.play(this.state.currentTime)
+    const pause = () => {
+        setPlaying(false)
+        replayer.current!.pause()
     }
 
-    pause = () => {
-        this.setState({ playing: false })
-        this.replayer.pause()
-    }
-
-    seek = (time: number) => {
-        this.replayer.play(time)
-        this.setState({ currentTime: time })
-        if (!this.state.playing) {
-            this.replayer.pause()
+    const seek = (time: number) => {
+        replayer.current!.play(time)
+        setCurrentTime(time)
+        if (!playing) {
+            replayer.current!.pause()
         }
     }
 
-    seekBack = () => {
-        this.seek(this.state.currentTime - JUMP_TIME_MS)
+    const seekBack = () => {
+        seek(currentTime - JUMP_TIME_MS)
     }
 
-    render = () => (
+    return (
         <div
             className='ph-rrweb-wrapper'
-            ref={this.wrapper}
-            onKeyDown={this.handleKeyDown}
+            ref={wrapper}
+            onKeyDown={handleKeyDown}
             tabIndex={0}
         >
             <div
                 className='ph-rrweb-frame'
-                ref={this.frame}
-                onClick={this.state.playing ? this.pause : this.play}
+                ref={frame}
+                onClick={playing ? pause : play}
             >
                 <div className='ph-rrweb-overlay'>
-                    <PlayPauseOverlay playing={this.state.playing} />
+                    <PlayPauseOverlay playing={playing} />
                 </div>
             </div>
             <div className='ph-rrweb-bottom'>
                 <div className='ph-rrweb-progress'>
                     <Slider
-                        value={this.state.currentTime}
+                        value={currentTime}
                         min={0}
-                        max={this.state.meta.totalTime}
+                        max={meta.totalTime}
                         step={0.01}
-                        onChange={this.seek}
+                        onChange={seek}
                     />
                 </div>
                 <div className='ph-rrweb-controller'>
@@ -189,14 +184,13 @@ export class Player extends PureComponent<Props, State> {
                             className: 'ph-rrweb-controller-icon'
                         }}
                     >
-                        {this.state.playing ? (
-                            <FaPause onClick={this.play} />
+                        {playing ? (
+                            <FaPause onClick={play} />
                         ) : (
-                            <FaPlay onClick={this.pause} />
+                            <FaPlay onClick={pause} />
                         )}
-                        <FaBackward onClick={this.seekBack} />
-                        {formatTime(this.state.currentTime)} /{' '}
-                        {formatTime(this.state.meta.totalTime)}
+                        <FaBackward onClick={seekBack} />
+                        {formatTime(currentTime)} / {formatTime(meta.totalTime)}
                     </IconContext.Provider>
                 </div>
             </div>
