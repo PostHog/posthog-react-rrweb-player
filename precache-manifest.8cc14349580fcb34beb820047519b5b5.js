277ec3dffeba7ca21a6847c00e8d776c6797d867
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-11-24 09:43:06 +0200

    Updates

---
+++ precache-manifest.8cc14349580fcb34beb820047519b5b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6201b95fbe3fcce29cdd874cacef111",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c98493fa30d5ac49e633",
+    "url": "./static/css/2.54e2e5f1.chunk.css"
+  },
+  {
+    "revision": "457b5adbe20b024273a5",
+    "url": "./static/css/main.56f5a9cc.chunk.css"
+  },
+  {
+    "revision": "c98493fa30d5ac49e633",
+    "url": "./static/js/2.4ba8ce3c.chunk.js"
+  },
+  {
+    "revision": "c738c3d897cc4d2d22a6da684fa71db9",
+    "url": "./static/js/2.4ba8ce3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457b5adbe20b024273a5",
+    "url": "./static/js/main.0bc5cec8.chunk.js"
+  },
+  {
+    "revision": "43a215b95067ee2bebea",
+    "url": "./static/js/runtime-main.4cfd5dec.js"
+  }
+]);
